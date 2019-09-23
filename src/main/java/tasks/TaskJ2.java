ea7ac832473abb36191455a2d528f96106950a40
TatyanaHerasymova <liketatyana@gmail.com>
2019-09-24 00:07:37 +0300

    new tasks

---
+++ src/main/java/tasks/TaskJ2.java
@@ -0,0 +1,11 @@
+public class TaskJ2 {
+    public static void main(String[] args) {
+        String name = "Tatyana";
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print(name + " ");
+            }
+            System.out.println();
+        }
+    }
+}
